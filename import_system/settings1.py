045980e119716365e1fb39328c5a3a7e7192db1e
damvict <damvict@gmail.com>
2025-10-12 15:54:00 +0530

    Updated REST framework settings for JWT auth

--- import_system/settings1.py
+++ import_system/settings1.py
@@ -110,8 +110,12 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
+
 
 
 # Password validation
