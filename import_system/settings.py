3c6287186586123a9ea623dce9cdad320994b83f
Damayanthi <your-email@example.com>
2025-12-31 14:23:11 +0530

    dashboad modi5

--- import_system/settings.py
+++ import_system/settings.py
@@ -143,11 +143,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Colombo'
+USE_TZ = True
+
 
 USE_I18N = True
 
-USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
