168fb5c6975fcb3f795c3774649503cd9d1988bb
Damayanthi <your-email@example.com>
2026-01-09 14:08:12 +0530

    shipment 17

--- masters/services.py
+++ masters/services.py
@@ -111,7 +111,7 @@ def update_shipment_stage(data, user):
                 defaults={
                    
                     #'bank_id': bank if bank else None ,                # ✅ FIXED
-                    'bank': 2,          # ✅ OBJECT
+                    'bank_id': 2,          # ✅ OBJECT
                     'currency': 1,
                     #'currency_id': shipment.currency_id,
                     'reference_number': data.get('reference_number'),
